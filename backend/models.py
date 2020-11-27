4c859e00f2bef950bcc89809c04aec7595601630
Vlad <vladlisin2@gmail.com>
2020-11-27 21:57:30 +0700

    admin

--- backend/models.py
+++ backend/models.py
@@ -9,15 +9,24 @@ class List(models.Model):
     name = models.CharField(max_length=200, default='')
     text = models.TextField(default='')
 
+    def __str__(self):
+        return self.name
+
 
 # Ontology
 class Person(models.Model):
     name = models.CharField(default='', max_length=300)
 
+    def __str__(self):
+        return self.name
+
 
 class System(models.Model):
     name = models.CharField(default='', max_length=300)
     description = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
 
 
 # Markup
